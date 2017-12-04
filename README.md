ec90db27e32daa492fcce8bb5dd93c385b738c8e
Bidhan Vaidya <bidhanvaidya@gmail.com>
2017-12-04 00:24:21 -0800

    Minor chnages to indentations and readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # ud036_StarterCode
-Source code for a Movie Trailer website.
-To run the movies trailer website clone the repo and then run the python file entertainment_center.py which is inside the folder ud036_StarterCode.
+To run the movies trailer website clone the repo and then run the python file entertainment_center.py..
+
+This webpage will contain the list of movies along with the poster image and when clicked on a any particular poster image,
+the trailer of that movie will be played.
+
--- entertainment_center.py
+++ entertainment_center.py
@@ -1,17 +1,14 @@
 import fresh_tomatoes
 import media
-
-
-
 deadpool2= media.Movie("DeadPool2",
-                 "https://upload.wikimedia.org/wikipedia/en/c/cf/Deadpool_2_poster.jpg",
-                 "https://www.youtube.com/watch?v=5V2X6cnSoxM")
+                       "https://upload.wikimedia.org/wikipedia/en/c/cf/Deadpool_2_poster.jpg",
+                       "https://www.youtube.com/watch?v=5V2X6cnSoxM")
 avengers= media.Movie("Avengers",
-                 "https://upload.wikimedia.org/wikipedia/en/9/90/Avengers_Infinity_War.jpg",
-                 "https://www.youtube.com/watch?v=6ZfuNTqbHE8")
+                      "https://upload.wikimedia.org/wikipedia/en/9/90/Avengers_Infinity_War.jpg",
+                      "https://www.youtube.com/watch?v=6ZfuNTqbHE8")
 black_panther= media.Movie("Black Panther",
-                     "https://upload.wikimedia.org/wikipedia/en/0/0c/Black_Panther_film_poster.jpg",
-                     "https://www.youtube.com/watch?v=vt9UZo32KMk")
+                           "https://upload.wikimedia.org/wikipedia/en/0/0c/Black_Panther_film_poster.jpg",
+                           "https://www.youtube.com/watch?v=vt9UZo32KMk")
 man_of_steel = media.Movie("Man of Steel 2",
                            "https://upload.wikimedia.org/wikipedia/en/8/85/ManofSteelFinalPoster.jpg",
                            "https://www.youtube.com/watch?v=dPl3_r9rf8E")
@@ -21,9 +18,6 @@ alpha = media.Movie("Alpha",
 dragonheart= media.Movie("DRAGONHEART",
                          "https://upload.wikimedia.org/wikipedia/en/1/18/Dragonheart_ver1.jpg",
                          "https://www.youtube.com/watch?v=1j1gfHwLICo")
-
-
-movies_list=[deadpool2, avengers, black_panther,
-             man_of_steel, alpha, dragonheart] #array of movies
+movies_list=[deadpool2, avengers, black_panther, man_of_steel, alpha, dragonheart] #array of movies
 
 fresh_tomatoes.open_movies_page(movies_list) # creating web page using list of movies from movie_list
