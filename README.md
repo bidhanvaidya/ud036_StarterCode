64d584ba74cba8c18d904e60a6a446cec9928eca
Bidhan Vaidya <bidhanvaidya@gmail.com>
2017-12-03 17:30:05 -0800

    Movie Trailer Website first copy

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ud036_StarterCode
 Source code for a Movie Trailer website.
+To run the movies trailer website clone the repo and then run the python file entertainment_center.py which is inside the folder ud036_StarterCode.
---
+++ entertainment_center.py
@@ -0,0 +1,29 @@
+import fresh_tomatoes
+import media
+
+
+
+deadpool2= media.Movie("DeadPool2",
+                 "https://upload.wikimedia.org/wikipedia/en/c/cf/Deadpool_2_poster.jpg",
+                 "https://www.youtube.com/watch?v=5V2X6cnSoxM")
+avengers= media.Movie("Avengers",
+                 "https://upload.wikimedia.org/wikipedia/en/9/90/Avengers_Infinity_War.jpg",
+                 "https://www.youtube.com/watch?v=6ZfuNTqbHE8")
+black_panther= media.Movie("Black Panther",
+                     "https://upload.wikimedia.org/wikipedia/en/0/0c/Black_Panther_film_poster.jpg",
+                     "https://www.youtube.com/watch?v=vt9UZo32KMk")
+man_of_steel = media.Movie("Man of Steel 2",
+                           "https://upload.wikimedia.org/wikipedia/en/8/85/ManofSteelFinalPoster.jpg",
+                           "https://www.youtube.com/watch?v=dPl3_r9rf8E")
+alpha = media.Movie("Alpha",
+                    "https://upload.wikimedia.org/wikipedia/en/7/72/Alpha_%28film%29.jpg",
+                    "https://www.youtube.com/watch?v=uIxnTi4GmCo")
+dragonheart= media.Movie("DRAGONHEART",
+                         "https://upload.wikimedia.org/wikipedia/en/1/18/Dragonheart_ver1.jpg",
+                         "https://www.youtube.com/watch?v=1j1gfHwLICo")
+
+
+movies_list=[deadpool2, avengers, black_panther,
+             man_of_steel, alpha, dragonheart] #array of movies
+
+fresh_tomatoes.open_movies_page(movies_list) # creating web page using list of movies from movie_list
